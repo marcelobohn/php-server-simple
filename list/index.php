c874787e65211b4b2b73a6da3eac205d10fafe5a
unknown <marcelo.bohn@N014.digitaldesk.com.br>
2016-05-04 10:23:08 -0300

    inclusão de novos produtos

--- list/index.php
+++ list/index.php
@@ -1,7 +1,10 @@
 <?php
 header('Content-type: application/json');
 $data = array();
-array_push($data, array('id' => 1, 'nome' => 'banana', 'valor' => 1.25));
-array_push($data, array('id' => 5, 'nome' => 'maca', 'valor' => 5.50));
+array_push($data, array('chave' => 1, 'nome' => 'banana', 'valor' => 1.25, 'staus' => 'A', 'estoque' => 20));
+array_push($data, array('chave' => 2, 'nome' => 'bergamota', 'valor' => 2.25, 'staus' => 'A', 'estoque' => 40));
+array_push($data, array('chave' => 3, 'nome' => 'laranja', 'valor' => 3.25, 'staus' => 'A', 'estoque' => 50));
+array_push($data, array('chave' => 4, 'nome' => 'abacaxi', 'valor' => 3.75, 'staus' => 'A', 'estoque' => 15));
+array_push($data, array('chave' => 5, 'nome' => 'maca', 'valor' => 5.50, 'staus' => 'I', 'estoque' => 10));
 echo json_encode($data);
 ?>
--- product/index.php
+++ product/index.php
@@ -4,10 +4,19 @@ parse_str($_SERVER['QUERY_STRING'], $params);
 $data = array();
 switch ($params['chave']) {
     case '1':
-        $data = array('chave' => 1, 'nome' => 'banana', 'valor' => 1.25);
+        $data = array('chave' => 1, 'nome' => 'banana', 'valor' => 1.25, 'staus' => 'A', 'estoque' => 20);
+        break;
+    case '2':
+        $data = array('chave' => 2, 'nome' => 'bergamota', 'valor' => 2.25, 'staus' => 'A', 'estoque' => 40);
+        break;
+    case '3':
+        $data = array('chave' => 3, 'nome' => 'laranja', 'valor' => 3.25, 'staus' => 'A', 'estoque' => 50);
+        break;
+    case '4':
+        $data = array('chave' => 4, 'nome' => 'abacaxi', 'valor' => 3.75, 'staus' => 'A', 'estoque' => 15);
         break;
     case '5':
-        $data = array('chave' => 5, 'nome' => 'maca', 'valor' => 5.50);
+        $data = array('chave' => 5, 'nome' => 'maca', 'valor' => 5.50, 'staus' => 'I', 'estoque' => 10);
         break;
 }
 echo json_encode($data);
